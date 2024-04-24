c39bd073071ab8d4ec652ee30a534d102d9822c3
JIbryll Brinkley <49615544+BryllieBrinkley@users.noreply.github.com>
2024-04-24 15:01:43 -0400

    optimized and refactored

--- ARRuler/ViewController.swift
+++ ARRuler/ViewController.swift
@@ -10,14 +10,16 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
-    var dotNodes = [SCNNode]()
     
     @IBOutlet var sceneView: ARSCNView!
+    
+    var dotNodes = [SCNNode]()
+    var textNode = SCNNode()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set the view's delegate
         sceneView.delegate = self
         
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
@@ -28,7 +30,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -42,71 +44,74 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+        if dotNodes.count >= 2 {
+            for dot in dotNodes {
+                dot.removeFromParentNode()
+            }
+            dotNodes = [SCNNode]()
+        }
+        
         if let touchLocation = touches.first?.location(in: sceneView) {
             let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
             
             if let hitResult = hitTestResults.first {
                 addDot(at: hitResult)
             }
-        }
-    }
-    
-    func addDot(at hitResult: ARHitTestResult) {
-        
-        let dotGeometry = SCNSphere(radius: 0.005)
-        let material = SCNMaterial()
-        material.diffuse.contents = UIColor.red
-        
-        dotGeometry.materials = [material]
-        
-        let dotNode = SCNNode(geometry: dotGeometry)
-        
-        dotNode.position = SCNVector3(x: hitResult.worldTransform.columns.3.x, y: hitResult.worldTransform.columns.3.y , z: hitResult.worldTransform.columns.3.z)
-
-        sceneView.scene.rootNode.addChildNode(dotNode)
-        dotNodes.append(dotNode)
-        
-        
-        if dotNodes.count > 2 {
-            calculate()
-        } else {
             
         }
     }
     
-    func calculate() {
+    func addDot(at hitResult : ARHitTestResult) {
+        
+        let dotGeometry = SCNSphere(radius: 0.005)
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.orange
+        dotGeometry.materials = [material]
+        let dotNode = SCNNode(geometry: dotGeometry)
+        
+        dotNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.z)
+        
+        sceneView.scene.rootNode.addChildNode(dotNode)
+        
+        dotNodes.append(dotNode)
+        
+        if dotNodes.count >= 2 {
+            calculate()
+        }
+    }
+                                                                                                         
+    func calculate (){
         let start = dotNodes[0]
         let end = dotNodes[1]
         
         print(start.position)
         print(end.position)
         
-        let a = end.position.x - start.position.x
-        let b = end.position.y - start.position.y
-        let c = end.position.z - start.position.z
+        let distance = sqrt(
+            pow(end.position.x - start.position.x, 2) +
+            pow(end.position.y - start.position.y, 2) +
+            pow(end.position.z - start.position.z, 2)
+        )
         
-        let distance = sqrt(pow(a, 2) + pow(b,2) + pow (c,2))
+        let distanceInInches = (distance / 2.54) * 100
+        updateText(text: "\(distanceInInches)", atPosition: end.position)
         
-        updateText(text: "\(abs(distance))")
+        //        distance = √ ((x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2)
+        
     }
     
-//        distance =  √ ((x2-x1)^2 + (y2-y1)^2 + (z2 - z1)^2)
-
-    func updateText(text:String) {
+    func updateText(text: String, atPosition position: SCNVector3){
         
-        let textGeometry = SCNText(string: text, extrusionDepth: 0.5)
-        
-        textGeometry.firstMaterial?.diffuse.contents = UIColor.black
-        
-        let textNode = SCNNode(geometry: textGeometry)
-        
-        textNode.position = SCNVector3(0, 0.01, -0.1)
-        
+        textNode.removeFromParentNode()
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.orange
+        textNode = SCNNode(geometry: textGeometry)
+        textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
         textNode.scale = SCNVector3(0.01, 0.01, 0.01)
-        
         sceneView.scene.rootNode.addChildNode(textNode)
         
     }
     
-
+    
 }
